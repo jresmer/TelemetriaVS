b9ea71e46ad0fc369a363a3b2956b3a4ab9c8d67
jresmer <jpresmer@gmail.com>
2024-12-11 19:39:58 +0000

    initial commit

---
+++ batteryEstimation/linear_regression_svm.c
@@ -0,0 +1,25 @@
+// Hard Margin SVM model for battery estimation based on read voltage (solved through the dual formulation)
+
+// computes the doct product between a and b, where a and b are vectors of size "size" and stores the result onto the variable "result"
+// will be used as the kernel function k(xn, xm)
+void dotProduct (float* a, float* b, size_t size, float* result) {
+    (* result) = 0;
+    for (size_t i = 0; i < size; i++)
+        (* result) += a[i] * b[i];
+}
+
+// computes the resulting vector of the nonlinear transformation ϕ(x)
+// will be used to enable the model to pick from a hypothesis set where the relation between input and output is nonlinear
+/* 
+two good possible hypothesis sets are:
+h(x) = w1 + w2.x1 + w3.x1^2
+h(x) = w1 + w2.e^-x1
+*/
+void linearTransformation(float* x, float** result) {}
+
+// optimizes the lagrange multipliers in order to train the model
+void train(float* a, float* x, float* y) {}
+
+void main() {
+
+}
