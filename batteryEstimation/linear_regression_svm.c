1fd9ab1624ac32d9e5694708fd3ab847ba7a7cfb
jresmer <jpresmer@gmail.com>
2024-12-16 19:53:05 +0100

    prediction function implemented

--- batteryEstimation/linear_regression_svm.c
+++ batteryEstimation/linear_regression_svm.c
@@ -10,10 +10,12 @@ subjecto to: ∑((αi - αi*)) = 0 and αi, αi* ∈ [0, C]
 #include <stdlib.h>
 #include <stdbool.h>
 
-void dotProduct (float* a, float* b, unsigned int d, float* result) {
-    (* result) = 0;
+float dotProduct (float* a, float* b, unsigned int d) {
+    float result = 0;
     for (int i = 0; i < d; i++)
-        (* result) += a[i] * b[i];
+        result += a[i] * b[i];
+    
+    return result;
 }
 
 // checks if lagrange multiplier a follows the kkt conditions
@@ -35,6 +37,32 @@ bool kkt(float* w, float a, float* x, float y, float c, float error) {
         followsKKT = yhxn <= error && yhxn >= 0;
 
     return followsKKT;
+}
+
+// estimates the output yi of input xi through the model
+/* 
+h(xi) = ∑(αn - αn*).k(xi, xn) + b, where n ∈ S
+to estimate b take one suport vector m
+b = ym - ∑(αn - αn*).k(xm, xn)
+*/ 
+// TODO - review kernels
+float predict(float** x, float* y, float* a, float* a_, int i, int d, int* s, int n_sv) {
+    // estimating b
+    int m = s[0];
+    float b = y[m];
+    for (int k = 0; k < n_sv; k++) {
+        int n = s[k];
+        b -= (a[n] - a_[n])*dotProduct(x[m], x[n], d);
+    }
+
+    float yi = 0;
+    for (int k = 0; k < n_sv; k++) {
+        int n = s[k];
+        yi += (a[n] - a_[n])*dotProduct(x[i], x[n], d);
+    }
+    yi += b;
+
+    return yi;
 }
 
 void main () {
