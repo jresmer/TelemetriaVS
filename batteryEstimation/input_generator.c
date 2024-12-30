dba06d35d7a0fbebf84cabc7965a3f592a7b8f76
jresmer <jpresmer@gmail.com>
2024-12-30 14:06:47 -0300

    initial commit

---
+++ batteryEstimation/input_generator.c
@@ -0,0 +1,67 @@
+/*
+This scripts generates labeled data that will be used for testing the regression models before the actual data is collected
+*/
+#include <math.h>
+
+void write_data (float** x, float* y, float* w, int size, int d, int dataset_size) {
+
+    FILE* file;
+    file = fopen("labeled_data.txt", "ab");
+    if (file == NULL) {
+        fprintf(stderr, "\nError opening file\n");
+        exit(1);
+    }
+    int stored_values = 0;
+    int flag;
+    // write w1 value to file labeled_data.txt
+    for (int i = 0; i < size; i++) {
+        flag = fwrite(&w[i], sizeof(float), 1, file);
+        if (!flag) {
+            printf("Error storing weights\n");
+            exit(1);
+        }
+    }
+    // writing training example x1 to file labeled_data.txt
+    for (int i = 0; i < dataset_size; i++) {
+        flag = fwrite(&x[i], sizeof(float), d, file);
+        if (!flag) {
+            printf("Error storing input data at %dth training example\n", stored_values + 1);
+            exit(1);
+        }
+        // wring target value yi to file labeled_data.txt
+        flag = fwrite(&y[i], sizeof(float), 1, file);
+        if (!flag) {
+            printf("Error storing input data at %dth training example\n", stored_values + 1);
+            exit(1);
+        }
+        stored_values++;
+    }
+}
+
+void generate_quadratic_data () {
+    float w[2] = {rand() % 1000 / rand() % 1000, rand() % 1000 / rand() % 1000};
+    float x[1000][1];
+    float y[1000];
+
+    for (int i = 0; i < 1000; i++) {
+        x[i][0] = (i - 500) / 200;
+        y[i] = w[0] + w[1] * pow(x[i][0], 2);
+    }
+
+    write_data(x, y, w, 2, 1, 1000);
+}
+
+void generate_exponential_data () {
+    float w[3] = {rand() % 1000 / rand() % 1000, rand() % 1000 / rand() % 1000, rand() % 1000 / rand() % 1000};
+    float x[1000][1];
+    float y[1000];
+
+    for (int i = 0; i < 1000; i++) {
+        x[i][0] = (i - 500) / 200;
+        y[i] = w[0] + w[1] * exp(w[2] * x[i][0]);
+    }
+
+    write_data(x, y, w, 3, 1, 1000);
+}
+
+int main () {raturn 1;}
